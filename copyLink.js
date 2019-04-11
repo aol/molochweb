16cd523bec8b404b9ccbe644963afd5530adb1f3
31453 <elyse.rinne@verizonmedia.com>
2019-04-11 11:17:29 -0400

    add settings page

---
+++ copyLink.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function copyLink (event, page) {
+  let copyText = `https://molo.ch/${page}#${event.parentNode.id}`;
+  // create an input to copy from
+  let input = document.createElement('input');
+  document.body.appendChild(input);
+  input.value = copyText;
+  input.select();
+  document.execCommand('copy', false);
+  input.remove();
+}
